dc483b90ad14a6ee9cac800a4964c03151a1eb24
florian.chanioux <florian.chanioux@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-08 13:26:48 +0000

    bug sur freebsd regler

--- trunk/srcs/serveur/serveur_map.c
+++ trunk/srcs/serveur/serveur_map.c
@@ -5,7 +5,7 @@
 ** Login   <chanio_f@epitech.net>
 ** 
 ** Started on  Wed Apr  7 18:05:33 2010 Florian Chanioux
-** Last update Thu Apr  8 14:38:14 2010 Florian Chanioux
+** Last update Thu Apr  8 15:19:34 2010 Florian Chanioux
 */
 
 #include <unistd.h>
@@ -38,11 +38,11 @@ static t_map 	*init_case(t_game *game, int orient, int x, int y)
   xf = x + card[orient].x ;
   if (xf < 0)
     xf = game->server.width - 1;
-  else if (xf > game->server.width - 1)
+  else if (xf >= game->server.width)
     xf = 0;
   if (yf < 0)
     yf = game->server.height - 1;
-  else if (yf > game->server.height - 1)
+  else if (yf >= game->server.height)
     yf = 0;
   printf("pos o x y: %i %i %i\n%x\n", orient, xf, yf, (int)&game->map[yf][xf]);
   return (&game->map[yf][xf]);
@@ -54,7 +54,7 @@ void		init_map(t_game *game)
   int		y;
   int		i;
 
-z  game->map = xmalloc(sizeof(game->map) * (game->server.height));
+  game->map = xmalloc(sizeof(game->map) * (game->server.height));
   y = -1;
   while (++y < game->server.height)
     game->map[y] = xmalloc(sizeof(game->map[y]) * (game->server.width));
@@ -109,7 +109,6 @@ void		free_map(t_game *game)
   while (y < game->server.height)
   {
     printf("y=%i\n", y);
-    
     free(game->map[y]);
     y++;
     puts("toto");
