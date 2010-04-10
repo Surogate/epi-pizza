0c758435747c0ab27fc8e2cf61af761ba4edb42c
aklarel <aklarel@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-10 16:48:36 +0000

    parser pour le serv

---
+++ inc/pars_init_teams.h
@@ -0,0 +1,13 @@
+/*
+**
+*/
+
+#ifndef			H_INIT_TEAMS_INCLUDED
+# define		H_INIT_TEAMS_INCLUDED
+
+#include		"t_struct.h"
+
+void			create_new_team(t_opt *opt, char *team_name);
+void			init_teams(t_opt *opt, char **av, int ac, int *i);
+
+#endif			/*H_INIT_TEAMS_INCLUDED*/
