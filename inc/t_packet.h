8fd242f07462796d66c0fb3ddd73c8170006ebad
FrancoisAncel <FrancoisAncel@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-11 13:06:21 +0000

    oublie du'un fichier

---
+++ inc/t_packet.h
@@ -0,0 +1,36 @@
+/*
+** t_packet for project in /u/all/ancel_a/cu/travail/c/
+**
+** Made by francois1 ancel
+** Login   <ancel_a@epitech.net>
+**
+** Started on  Sun Apr 11 14:39:48 2010 francois1 ancel
+** Last update Sun Apr 11 14:39:48 2010 francois1 ancel
+*/
+
+#ifndef			T_PACKET_INCLUDED
+# define		T_PACKET_INCLUDED
+
+typedef struct
+{
+  int			id_player;
+  char			*mess;
+}			t_rep;
+
+typedef struct
+{
+  int			player_id;
+  int			ac;
+  char			*av[2];
+  struct timeval	time;
+  int			duration;
+  t_rep			*response; /* tableau */
+}			t_packet;
+
+typedef struct
+{
+  char			*inst;
+  void			(*ptr_func)();
+}			t_inst;
+
+#endif			/* !T_PACKET_INCLUDED */
