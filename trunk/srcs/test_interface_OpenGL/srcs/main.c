f59e286af9b66aef438ce12ffcc96c50ce458678
florian.chanioux <florian.chanioux@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-14 11:39:34 +0000

    opengl mis en place c'est un peu lent , c'est pas propre, mais c'est un debut

--- trunk/srcs/test_interface_OpenGL/srcs/main.c
+++ trunk/srcs/test_interface_OpenGL/srcs/main.c
@@ -5,7 +5,7 @@
 ** Login   <pierro_a@epitech.net>
 **
 ** Started on  Sun Apr  4 17:38:25 2010 frederic1 pierronnet
-** Last update Mon Apr  5 00:06:03 2010 frederic1 pierronnet
+** Last update Wed Apr 14 15:35:03 2010 Florian Chanioux
 */
 #include	<SDL/SDL.h>
 #include	<SDL/SDL_ttf.h>
@@ -17,43 +17,42 @@
 #include	"../includes/struct.h"
 #include	"../includes/proto.h"
 
-t_event		event_type[]=
-{
-  {SDL_QUIT, exit_func},
-  {SDL_MOUSEBUTTONDOWN, mouse_down},
-  {SDL_MOUSEBUTTONUP, mouse_up},
-  {SDL_MOUSEMOTION, mouse_move},
-  {SDL_KEYDOWN, key_func},
-  {0,0}
-};
-
 int		main(void)
 {
   t_game	game;
   int		i;
 
   init_game(&game);
+  initAttributes();
+  /* createSurface(0); Create GL context */
+  printAttributes();
+  initGL();
+  mainloop(&game);
+  SDL_Quit();
+
+  /*
   while (1)
+  {
+    if (SDL_PollEvent(&game.event))
     {
-      if (SDL_PollEvent(&game.event))
-	{
-	  i = 0;
-	  while (event_type[i].type)
-	    {
-	      if (game.event.type == event_type[i].type)
-	      event_type[i].func(&game);
-	      i++;
-	    }
-	}
-      SDL_FillRect(game.screen, NULL,
-		   SDL_MapRGB(game.screen->format, 0, 0, 0));
-      display_map(&game);
-      display_mouse(&game);
-      if (SDL_Flip(game.screen) == -1)
-	{
-	  printf("Refresh fail\n");
-	  exit(EXIT_FAILURE);
-	}
-     }
+      i = 0;
+      while (event_type[i].type)
+      {
+	if (game.event.type == event_type[i].type)
+	  event_type[i].func(&game);
+	i++;
+      }
+    }
+    SDL_FillRect(game.screen, NULL,
+		 SDL_MapRGB(game.screen->format, 0, 0, 0));
+    display_map(&game);
+    display_mouse(&game);
+    if (SDL_Flip(game.screen) == -1)
+    {
+      printf("Refresh fail\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  */
   return (EXIT_SUCCESS);
 }
