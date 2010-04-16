bc2ec811f5e8560b71f5799cc82fc73de6c5b1e6
florian.chanioux <florian.chanioux@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-16 12:18:43 +0000

    fog

--- srcs/test_interface_OpenGL/srcs/glmain.c
+++ srcs/test_interface_OpenGL/srcs/glmain.c
@@ -5,7 +5,7 @@
 ** Login   <chanio_f@epitech.net>
 ** 
 ** Started on  Wed Apr 14 14:36:35 2010 Florian Chanioux
-** Last update Fri Apr 16 15:59:51 2010 Florian Chanioux
+** Last update Fri Apr 16 16:17:35 2010 Florian Chanioux
 */
 
 #include <stdio.h>
@@ -63,7 +63,7 @@ void		initAttributes()
 
 void Init( void)
 {
-
+/*
   static float ambient[]= {0.2, 0.2, 0.2, 1.0};
   static float diffuse[]= {1.0, 1.0, 1.0, 1.0};
   static float position[]= {0.0, 1.0, 0.0, 0.0};
@@ -73,9 +73,9 @@ void Init( void)
   static float mat_ambient[]= {0.3, 0.4, 0.5, 1.0};
   static float lmodel_ambient[]= {0.4, 0.4, 0.4, 1.0};
   static float lmodel_localviewer[]= {0.0};
-  /*GLfloat map1[4] = {0.0, 0.0, 0.0, 0.0};
-    GLfloat map2[4] = {0.0, 0.0, 0.0, 0.0};*/
-  static float fog_color[]= {0.0, 0.5, 0.9, 1.0};
+  GLfloat map1[4] = {0.0, 0.0, 0.0, 0.0};
+  GLfloat map2[4] = {0.0, 0.0, 0.0, 0.0};*/
+  /*
   glFrontFace( GL_CCW);
   glDepthFunc( GL_LESS);
   glEnable( GL_DEPTH_TEST);
@@ -90,11 +90,19 @@ void Init( void)
   glMaterialfv( GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
   glMaterialfv( GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
   glMaterialfv( GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
-  glEnable( GL_FOG);
-  glFogi(GL_FOG_MODE, GL_EXP);
-  glFogf(GL_FOG_DENSITY, 0.0000025);
-  glFogfv(GL_FOG_COLOR, fog_color);
-  glClearColor(0.0, 0.5, 0.9, 1.0);
+*/
+  static float fog_color[]= {0.5, 0.5, 0.5, 1.0};
+  GLint fogmode = GL_EXP ;
+
+  glEnable( GL_FOG );
+  glFogi (GL_FOG_MODE, fogmode) ;
+  glFogfv(GL_FOG_COLOR, fog_color) ;
+  glFogf(GL_FOG_DENSITY, 0.01) ;
+  glFogf(GL_FOG_START, 1.0) ;
+  glFogf(GL_FOG_END, 5.0) ;
+
+  glClearColor( 0.5, 0.5, 0.5, 1.0 );
+  glEnable( GL_DEPTH_TEST );
 
 }
 
