4e5c4618083f53f2144c9b42485eb9da2e8052b9
florian.chanioux <florian.chanioux@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-20 18:34:35 +0000

    pathfinding operationelle opengl loader 3d pas encore pres

--- trunk/srcs/serveur/test_map.c
+++ trunk/srcs/serveur/test_map.c
@@ -5,7 +5,7 @@
 ** Login   <chanio_f@epitech.net>
 ** 
 ** Started on  Fri Apr  9 15:22:12 2010 Florian Chanioux
-** Last update Thu Apr 15 19:06:29 2010 Florian Chanioux
+** Last update Tue Apr 20 20:48:31 2010 Florian Chanioux
 */
 
 #include <unistd.h>
@@ -23,13 +23,16 @@ void		test_aff_map(t_game *game)
   int		x;
   int		y;
 
-  y = -1;
-  while (++y < game->server.height)
+  if (game->server.debug)
   {
-    x = -1;
-    while (++x < game->server.width)
-      printf("%p ", (void *)&(game->map[y][x]));
-    printf("\n");
+    y = -1;
+    while (++y < game->server.height)
+    {
+      x = -1;
+      while (++x < game->server.width)
+	printf("%p ", (void *)&(game->map[y][x]));
+      printf("\n");
+    }
   }
 }
 
@@ -40,25 +43,27 @@ void		test_map(t_game *game)
   int		y;
   t_map		*cas;
 
-  y = -1;
-  while (++y < game->server.height)
+  if (game->server.debug)
   {
-    x = -1;
-    while (++x < game->server.width)
+    y = -1;
+    while (++y < game->server.height)
     {
-      i = -1;
-      printf("y: %i, x: %i case: %p\n", y, x, (void *)&(game->map[y][x]));
-      cas = &(game->map[y][x]);
-      while (++i < M_CARD)
-        printf("card : %i = %p\n", i, (void *)cas->card[i]);
-      i = -1;
-      printf("#cout : = %i\n\n", cas->cout);
-      while (++i < RESS_NUM)
-        printf("##res : %i = %i\n", i, cas->cas.ress[i]);
-      printf("#nb_player : = %i\n\n", my_l_size(cas->cas.player));
+      x = -1;
+      while (++x < game->server.width)
+      {
+	i = -1;
+	printf("y: %i, x: %i case: %p\n", y, x, (void *)&(game->map[y][x]));
+	cas = &(game->map[y][x]);
+	while (++i < M_CARD)
+	  printf("card : %i = %p\n", i, (void *)cas->card[i]);
+	i = -1;
+	printf("#cout : = %i\n\n", cas->cout);
+	while (++i < RESS_NUM)
+	  printf("##res : %i = %i\n", i, cas->cas.ress[i]);
+	printf("#nb_player : = %i\n\n", my_l_size(cas->cas.player));
+      }
     }
-  }
-}
+  }}
 
 void		test_pathfinding_map(t_game *game)
 {
@@ -66,15 +71,18 @@ void		test_pathfinding_map(t_game *game)
   int		y;
   t_map		*cas;
 
-  y = -1;
-  while (++y < game->server.height)
+  if (game->server.debug)
   {
-    x = -1;
-    while (++x < game->server.width)
+    y = -1;
+    while (++y < game->server.height)
     {
-      cas = &(game->map[y][x]);
-      printf("%i ", cas->cout);
+      x = -1;
+      while (++x < game->server.width)
+      {
+	cas = &(game->map[y][x]);
+	printf("%i ", cas->cout);
+      }
+      printf("\n");
     }
-    printf("\n");
   }
 }
