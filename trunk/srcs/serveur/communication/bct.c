43f747b27d51e4161270236f25a3c3839d87d73e
FrancoisAncel <FrancoisAncel@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-22 10:39:44 +0000

    correction de l'envois a la connection du graph

--- trunk/srcs/serveur/communication/bct.c
+++ trunk/srcs/serveur/communication/bct.c
@@ -17,8 +17,8 @@ char		*bct(char *msg, t_map *map)
   int           i;
 
   i = -1;
-  msg = xrealloc(msg, (strlen(msg) + 116) * sizeof(char));
-  snprintf(msg + strlen(msg), 4, "bct %i %i", map->x, map->y);
+  msg = xrealloc(msg, (strlen(msg) + 28 + (11 * RESS_NUM)) * sizeof(char));
+  snprintf(msg + strlen(msg), 27, "bct %i %i", map->x, map->y);
   while (++i != RESS_NUM)
     snprintf(msg + strlen(msg), 11, " %i", map->cas.ress[i]);
   snprintf(msg + strlen(msg), 2, "\n");
