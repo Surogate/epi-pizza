79bac636de7208acb01330c864f0c64755258617
pierre.boutbel <pierre.boutbel@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-26 15:33:42 +0000

    Ajout de headers

--- srcs/serveur/communication/grp_move.c
+++ srcs/serveur/communication/grp_move.c
@@ -1,5 +1,11 @@
 /*
-**  Header
+** concatene_msg_view.c for zappy in /u/all/boutbe_a/cu/rendu/c/projets/epi-pizza/srcs/serveur/jeu
+**
+** Made by pierre1 boutbel
+** Login   <boutbe_a@epitech.net>
+**
+** Started on  Wed Apr 14 13:18:55 2010 pierre1 boutbel
+** Last update Sat Apr 24 05:50:01 2010 Florian Chanioux
 */
 
 #include	<sys/types.h>
