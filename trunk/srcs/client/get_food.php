a4d201bb4192489b0c92812f5b088a30dc9df562
aklarel <aklarel@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-26 22:38:17 +0000

    lulz

---
+++ trunk/srcs/client/get_food.php
@@ -0,0 +1,14 @@
+<?php
+
+function		get_food(&$player)
+{
+  $inv = $player['inv'];
+  $inv = str_replace('{', '', $inv);
+  $inv = str_replace('}', '', $inv);
+  $inv = explode(', ', $inv);
+
+  $food = explode(' ', $inv[0]);
+  $player['food'] = intval($food[1]);
+}
+
+?>
