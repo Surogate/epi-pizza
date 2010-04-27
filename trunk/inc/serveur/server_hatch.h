f7199d6eb9f4161d4bf502ad9be4495ba7f11b55
FrancoisAncel <FrancoisAncel@a47ccae1-dada-09be-baf4-afd030c0c0f0>
2010-04-27 15:10:35 +0000

    correction et ajout de message

--- trunk/inc/serveur/server_hatch.h
+++ trunk/inc/serveur/server_hatch.h
@@ -15,6 +15,6 @@ int		find_hatch_fct(t_packet *in, int *player_id);
 int	       	create_hatch(t_svr_vector *vec, t_packet *tmp);
 int	       	server_hatch(t_svr_vector *vec, t_packet *pak, t_game *game);
 void		delete_hatch(t_svr_vector *vec, int player_id);
-void		do_hatch(t_game *game, int id_egg);
+void		do_hatch(t_game *game, int id_egg, t_svr_vector *vec);
 
 #endif
